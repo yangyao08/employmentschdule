1af70e8ca0f44bc2d98dc840969525a590443eb3
soh yangyao <syy.yangyao@gmail.com>
2018-06-04 23:45:01 +0800

    my name is jeff

--- ScheduleConvertV2.py
+++ ScheduleConvertV2.py
@@ -14,7 +14,7 @@ import datetime
 import random
 
 
-engine = create_engine('sqlite:///C:\\Users\\yangyao\\Desktop\\Scheduling Project\\ScheduleV2.db', echo=True)
+engine = create_engine('sqlite:///C:\\Users\\yangyao\\Desktop\\Scheduling Project\\ScheduleV2.db', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 s = Session() #Connection to the system
@@ -31,15 +31,17 @@ connection = Table('connect',Base.metadata,\
                    Column('person_id', Integer, ForeignKey('people.id')),\
                    Column('engage_id', Integer, ForeignKey('jobs.id')))
 
+###################################################################################################################
+
 class Personnel(Base):
     #run once is sufficient
     __tablename__ = 'people'
     
     id = Column(Integer,primary_key = True)
-    name = Column(String(50),unique = True) #unique? #in the instance someone have different name, must save it differently
+    name = Column(String(50),unique = True)
     position = Column(String(50))
     #engagements = Column(ScalarListType(), nullable=True) #Why nullable? #does this work?
-    engagements = relationship('Engagement',secondary=connection, backref='personnels',lazy='dynamic')
+    engagements = relationship('Engagement',secondary=connection, backref='personnels',lazy='dynamic') ##self.engagements
     diff_1 = Column(Integer, default=0)
     diff_2 = Column(Integer, default=0)
     diff_3 = Column(Integer, default=0)
@@ -48,12 +50,62 @@ class Personnel(Base):
     def __init__(self,name,position):
         self.name = name
         self.position = position
-        ##MISSING
-        ##self.tier (removed)
-        ##self.engagements
-        ##self.distribution
+        
+    #Constraint 1
+    #Return a rating on how occupied a person is starting from a given date
+    def capacity(self,date):
+        relevant = []
+        for job in self.engagements:
+            if job.end>= date:
+                relevant.append(job) 
+        return sum(list(map(lambda x:x.hours,relevant)))
     
-         
+    #Constraint 2
+    #Return a rating of how busy a person is starting from a given date
+    def busyness(self,date):
+        busy = 0
+        for each in self.engagements:#########filtering not done yet######################!!!!!!!!!!!!!!!!!!
+            busyratio = each.hours/each.deadline #assume constant throughout all days
+            weight = 1/(abs((each.end - date).days)+1)
+            busy += busyratio*weight #+1 to prevent 
+        return busy
+    
+    #Constraint 3A
+    #Return a rating of the overall estimated difficulty of the jobs on-hand starting from a given date
+    def diff_scale(self,date):
+        count=0
+        for k,v in self.breakdown(date).items():
+            count+=k*v
+        return count
+    
+    #Constraint 3B
+    def breakdown(self,date):
+        distribution = {1:0,2:0,3:0} 
+        relevant = []
+        for job in self.engagements:
+            if job.end>= date:
+                relevant.append(job) 
+        for each in relevant:
+            distribution[each.difficulty] += 1 #not added in yet###################!!!!!!!!!!!!!!!!!!!!!!!!
+        return self.distribution
+    
+     """
+
+    def laidoff(self):  
+        s.delete(self)
+        s.commit()
+        
+      
+        if self.engagements:
+            for each in self.engagements:
+                each.personnels.remove(self)
+                each.requirement += 1
+        s.delete(self)
+        s.commit()
+        """
+
+################################################################################################################            
+        
 
 class Engagement(Base):
     #run once is sufficient
@@ -68,7 +120,6 @@ class Engagement(Base):
     deadline = Column(Integer)
     requirement = Column(Integer)
     #personnels = Column(ScalarListType(), nullable=True)
-    #personnels = relationship('Personnel',secondary=connection, backref = backref('engagements'))
     completed = Column(Boolean, default = False)
     
     def __init__(self,name,jobtype,hours,startdate,enddate,requirement):
@@ -86,10 +137,10 @@ class Engagement(Base):
     
 
 Base.metadata.create_all(engine)
-
-
-Adam = Personnel('Adam',"Associate")
+        
+#Base.metadata.drop_all(engine)
 """
+Adam = Personnel('Adam',"Associate")
 Brock = Personnel('Brock',"Senior Associate")
 Candy = Personnel('Candy',"Associate")
 
@@ -108,11 +159,24 @@ s.commit()
 s.add(ProjectB)
 s.commit()
 
-
 ProjectA.personnels.append(Adam)
 ProjectA.personnels.append(Candy)
 ProjectB.personnels.append(Adam)
 """
+
+#updatepeeps = s.query(Engagement).filter(Engagement.name == 'ProjectA').first()
+#updatepeeps.requirement -= 1
+#updatepeeps.hours -= 2.0
+#s.commit()
+
+
+
+
+
+
+
+
+
 
 
 
@@ -132,6 +196,7 @@ s.commit()
 # To retrieve it back, 
 # for smth in <Engagement>.personnels:
 # for smth i <Personnel>.engagements
+# Find out how store list of names in a column
 
 
     
