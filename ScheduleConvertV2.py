cb1844749a88bcd28be3c04bd1cf37b506220015
soh yangyao <syy.yangyao@gmail.com>
2018-06-08 00:46:56 +0800

    kukukukuku

--- ScheduleConvertV2.py
+++ ScheduleConvertV2.py
@@ -22,7 +22,7 @@ s = Session() #Connection to the system
 ###################################################################################################################
 #Assignment of difficulty to each type of job
 #Level of difficulty needs to be changed
-engagediff = {'channel 1':{'PPA':1,'WACC':1,'Biz Val':1,'Impair':1},'channel 2':{'PPA':2,\
+engagediff = {'C1':{'PPA':1,'WACC':1,'Biz Val':1,'Impair':1},'C2':{'PPA':2,\
               'Biz Val':2, 'BM Work':3, 'Others':3}}
 
 ###################################################################################################################
@@ -45,11 +45,13 @@ class Personnel(Base):
     diff_1 = Column(Integer, default=0)
     diff_2 = Column(Integer, default=0)
     diff_3 = Column(Integer, default=0)
-    left = Column(Boolean, default = False)
+    #Add list of engagements
+    
     
     def __init__(self,name,position):
         self.name = name
         self.position = position
+    
         
     #Constraint 1
     #Return a rating on how occupied a person is starting from a given date
@@ -66,8 +68,8 @@ class Personnel(Base):
         busy = 0
         for each in self.engagements:#########filtering not done yet######################!!!!!!!!!!!!!!!!!!
             busyratio = each.hours/each.deadline #assume constant throughout all days
-            weight = 1/(abs((each.end - date).days)+1)
-            busy += busyratio*weight #+1 to prevent 
+            weight = 1/(abs((each.end - date).days)+1) #+1 to prevent error
+            busy += busyratio*weight 
         return busy
     
     #Constraint 3A
@@ -86,11 +88,10 @@ class Personnel(Base):
             if job.end>= date:
                 relevant.append(job) 
         for each in relevant:
-            distribution[each.difficulty] += 1 #not added in yet###################!!!!!!!!!!!!!!!!!!!!!!!!
+            distribution[each.difficulty] += 1 
         return self.distribution
     
-     """
-
+"""
     def laidoff(self):  
         s.delete(self)
         s.commit()
@@ -102,7 +103,7 @@ class Personnel(Base):
                 each.requirement += 1
         s.delete(self)
         s.commit()
-        """
+"""
 
 ################################################################################################################            
         
@@ -114,40 +115,44 @@ class Engagement(Base):
     id = Column(Integer,primary_key = True)
     name = Column(String(100),unique = True)
     jobtype = Column(String(100))
+    difficulty = Column(Integer)
     hours = Column(Float)
     startdate = Column(DateTime)
     enddate = Column(DateTime)
     deadline = Column(Integer) #not calculated yet
     requirement = Column(Integer)
     #personnels = Column(ScalarListType(), nullable=True)
-    completed = Column(Boolean, default = False) ##self.completed
     
     def __init__(self,name,jobtype,hours,startdate,enddate,requirement):
+        jobtier = jobtype.split(':')
         self.name = name
         self.jobtype = jobtype
+        self.difficulty = engagediff[jobtier[0]][jobtier[1]]
         self.hours = hours
         self.startdate = startdate
         self.enddate = enddate
+        self.deadline = (enddate-startdate).days + 1
         self.requirement = requirement
-        ##MISSING
-        ##self.difficulty
-        #self.deadline
     
     def priority(self,person): #wnsure that it is peeps
         if person in self.personnels:
-          print(person.name + " has been assigned to this engagement")
+          return person.name + " has been assigned to this engagement"
         elif self.requirement == 0:
           print("Requirement has already been fulfilled")
         else:
           self.personnels.append(person)
           self.requirement -= 1
+          if self.difficulty == 1:
+              person.diff_1 += 1
+          elif self.difficulty == 2:
+              person.diff_2 += 1
+          else:
+              person.diff_3 += 1
           s.commit()
     
-    def assignment(self): #TBC
-        if self.requirement == 0:
-            print("Requirement has already been fulfilled")
+    def assignment(self): #alot of work to be done here. cant do the sort
         while self.requirement != 0:
-            all_personnel = s.query(Personnel).filter(Personnel not in self.personnels)
+            all_personnel = s.query(Personnel).filter(Personnel not in self.personnels) #error???!!!
             #filter bottom 50th percentile 
             ls=[]
             all_personnel.sort(key = lambda x: x.capacity(self.start))
@@ -162,21 +167,24 @@ class Engagement(Base):
             selected = random.choice(ls) #Random selection from resultant personnel
             self.requirement -= 1
             self.personnels.append(selected)
-            s.commit()
-       """ 
-    #Engagement has been completed 
-    ### Factor in force end or deadline end
-    def finish(self):
-        for peeps in self.involved:
-            peeps.engagements.remove(self) #remove the job
-        self.complete = True #should i del it instead? del self
-        """ 
-        #remove externally
+            if self.difficulty == 1:
+                selected.diff_1 += 1
+            elif self.difficulty == 2:
+                selected.diff_2 += 1
+            else:
+                selected.diff_3 += 1
+        print("Requirement has already been fulfilled")
+        s.commit()
         
-        def extend_deadline(self,new_end_date):
-            self.deadline += (new_end_date - self.end).days
+    #remove externally
+    def extend_deadline(self,new_end_date):
+        if new_end_date> self.enddate:
+            self.deadline += (new_end_date - self.enddate).days
             self.enddate = new_end_date
-            s.commit()
+        else:
+            print("Please check your new end date.")
+        s.commit()        
+    
 ################################################################################################################
 #Functions
             
@@ -186,8 +194,11 @@ def add_personnel(name,position):
     s.commit()
 
 def leave_personnel(name):
-    selected = s.query(Personnel).filter(Personnel.name == name).first()
+    selected = s.query(Personnel).filter(Personnel.name == name).first() 
+    for eachjob in selected.engagements:
+        eachjob.requirement += 1
     s.delete(selected) #need to commit?
+    s.commit()
 
 def add_engagement(name,jobtype,hours,startdate,enddate,requirement):
     engage = Engagement(name,jobtype,hours,startdate,enddate,requirement)
@@ -197,59 +208,100 @@ def add_engagement(name,jobtype,hours,startdate,enddate,requirement):
 def assign(engagename,*personname):
     eselected =  s.query(Engagement).filter(Engagement.name == engagename).first()
     if personname:
-        pselected = s.query(Personnel).filter(Personnel.name == personname).first()
-        eselected.priority(pselected)
-    eselected.assignment()
+        for each in personname:
+            pselected = s.query(Personnel).filter(Personnel.name == each).first()
+            eselected.priority(pselected)
+    eselected.assignment() #might need to do warning beforehand
 
 def pullout_personnel(engagename,personname):
     eselected =  s.query(Engagement).filter(Engagement.name == engagename).first()
     pselected = s.query(Personnel).filter(Personnel.name == personname).first()
-    if pselected.engagements not in eselected.personnels:
-        return "Error: Personnel not found in engagement" #can be better phrased
+    if pselected not in eselected.personnels:
+        print("Error: Personnel not found in engagement") #can be better phrased
     else:
+        if eselected.difficulty == 1:
+            pselected.diff_1 += 1
+        elif eselected.difficulty == 2:
+            pselected.diff_2 += 1
+        else:
+            pselecteds.diff_3 += 1
         eselected.personnels.remove(pselected)
         eselected.requirement += 1
         s.commit()
         
 def end_engagement(engagename):
     eselected = s.query(Engagement).filter(Engagement.name == engagename).first()
+    for eachpeeps in eselected.personnels:
+        if eselected.difficulty == 1:
+            eachpeeps.diff_1 += 1
+        elif eselected.difficulty == 2:
+            eachpeeps.diff_2 += 1
+        else:
+            eachpeeps.diff_3 += 1
     s.delete(eselected) #need to commit? relation broken? need to check
+    s.commit()
 
 def time_extension(engagename, new_date):
     eselected = s.query(Engagement).filter(Engagement.name == engagename).first()
     eselected.extend_deadline(new_date)
-    
-        
-        
-        
 
+def timecheck(): 
+        currentdate = datetime.date.today()
+        all_engagement = s.query(Engagement).all
+        for each in all_engagement:
+            if each.end < currentdate:   
+                end_engagement(each.name)
+                
+def fired(personname):
+    pselected = s.query(Personnel).filter(Personnel.name == personname).first()
+    for each in pselected.engagements:
+        each.requirement += 1
+    s.delete(pselected)
+    s.commit()
+        
 Base.metadata.create_all(engine)
         
 #Base.metadata.drop_all(engine)
+
 """
-Adam = Personnel('Adam',"Associate")
-Brock = Personnel('Brock',"Senior Associate")
-Candy = Personnel('Candy',"Associate")
+#Part1: Check add personel and engagement
+add_personnel('Adam','Associate')
+add_personnel('Brock','Senior Associate')
+add_personnel('Candy','Associate')
+add_personnel('Jamie','Associate')
 
-ProjectA = Engagement('ProjectA','C1:PPA',60,datetime.date(2018,6,1),datetime.date(2018,6,30),2)
-ProjectB = Engagement('ProjectB','C1:PPA',60,datetime.date(2018,6,1),datetime.date(2018,6,30),2)
-
-s.add(Adam)
-s.commit()
-s.add(Brock)
-s.commit()
-s.add(Candy)
-s.commit()
-
-s.add(ProjectA)
-s.commit()
-s.add(ProjectB)
-s.commit()
-
-ProjectA.personnels.append(Adam)
-ProjectA.personnels.append(Candy)
-ProjectB.personnels.append(Adam)
+add_engagement('ProjectA','C1:PPA',60,datetime.date(2018,6,1),datetime.date(2018,6,30),2)
+add_engagement('ProjectB','C2:PPA',60,datetime.date(2018,6,1),datetime.date(2018,6,30),2)
 """
+
+
+#Part2: Check that people can leave (w/o jobs on hand)
+#leave_personnel('Jamie')
+
+"""
+#Part3: Check assignment works.
+#Rmb to check if over assigment will not work
+assign('ProjectA','Adam','Brock')
+assign('ProjectB') 
+
+#Failed normal assignment
+"""
+
+"""
+#Part4: Check fired
+fired('Adam')
+add_personnel('Adam','Associate')
+"""
+#Part4: Pullout Personnel. CHeck if person not in project
+#pullout_personnel('ProjectA','Brock')
+
+"""
+#Part5: Time extension
+time_extension('ProjectA',datetime.datetime(2018,7,25))
+"""
+
+
+
 
 #updatepeeps = s.query(Engagement).filter(Engagement.name == 'ProjectA').first()
 #updatepeeps.requirement -= 1
@@ -261,29 +313,13 @@ ProjectB.personnels.append(Adam)
 
 
 
-
-
-
-
-
-
-
-"""
-
-Jamie = Personnel('Jamie',"Associate")
-#ProjectA = Engagement('ProjectA','C1:PPA',60,datetime.date(2018,6,1),datetime.date(2018,6,30),2)
-s.add(Jamie)
-s.commit()
-#s.add(ProjectA)
-#s.commit()
-"""
-
 ######################COMMENTS#####################
 # To establish the relation, <engagement>.personnels.append(<personnel>)
 # To retrieve it back, 
 # for smth in <Engagement>.personnels:
 # for smth i <Personnel>.engagements
 # Find out how store list of names in a column
+# Find out 
 
 
     
